d2878998e0b3519c428185ae1329f8af041a0b3c
Alan Jimenez <alanjq@outlook.com>
2024-06-17 12:53:56 -0500

    Actividad tema 1

---
+++ tema-1/actividad/post.php
@@ -0,0 +1,10 @@
+<?php 
+    $busqueda = $_POST['busqueda'];
+    echo $busqueda . "<br/>";
+
+    include 'resultados.html';
+    include 'resultados.html';
+    include 'resultados.html';
+    include 'resultados.html';
+?>
+
